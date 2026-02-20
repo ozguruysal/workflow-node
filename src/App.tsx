c4eaa730e63a1109b2ef3432405d9870a2955b9f
Özgür Uysal <ozguruysal@gmail.com>
2026-02-20 03:11:22 +0300

    Use button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,20 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import { Button } from "./design-system/components";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <Button onClick={() => setCount((count) => count + 1)}>
           count is {count}
-        </button>
+        </Button>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
