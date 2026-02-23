42cfcf4572e4522b19294f6c645f7b7f978f4cd7
Özgür Uysal <ozguruysal@gmail.com>
2026-02-23 18:30:28 +0300

    Deployment updates

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -4,5 +4,11 @@ const config: StorybookConfig = {
   stories: ["../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: ["@storybook/addon-a11y", "@storybook/addon-docs"],
   framework: "@storybook/react-vite",
+  viteFinal(config) {
+    return {
+      ...config,
+      base: "/storybook/",
+    };
+  },
 };
 export default config;
